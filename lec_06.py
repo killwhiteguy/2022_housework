41cdba58f3da25115dc336709f66eb3a9fe8276b
killwhiteguy <cannabanoid@mail.ru>
2022-10-17 11:26:19 +0300

    Сделал декомпозицию задачи, выделив функцию draw_picture.

--- lec_06.py
+++ lec_06.py
@@ -7,12 +7,25 @@ Created on Sun Oct 16 08:00:13 2022
 """
 
 import pygame
+
+def draw_picture(screen, x, y, width, height):
+        """  
+        Рисует картинку с домиком на фоне травы и неба.
+        
+        :param screen: дисплей pygame, на котором будет изображение
+        :param x: левая координата рисуемого прямоугольника с рисунком
+        :param y: верхняя координата прямоугольника с рисунком
+        :param widht: ширина прямоугольника
+        :param height: высота прямоугольника
+        """
+        print("Типа рисую картинку", x, y, width, height)
+
 width, height = screen_size = (500, 400)
 pygame.init()
 screen = pygame.display.set_mode(screen_size)
 
 # здесь будут рисоваться фигуры
-# draw_picture(screen)
+draw_picture(screen, 0, 0, width, height)
 # ...
 
 pygame.display.update()
